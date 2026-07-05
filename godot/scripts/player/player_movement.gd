84d1b833722b15226d0a6cd921328052f645815e
adamonine-stack <adamo.nine@gmail.com>
2026-07-05 18:35:22 +0900

    DEV-008 add player jump movement

--- godot/scripts/player/player_movement.gd
+++ godot/scripts/player/player_movement.gd
@@ -1,12 +1,25 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var move_speed := 300.0
+@export var jump_power := 500.0
 @export var screen_margin := 36.0
 
+var gravity := ProjectSettings.get_setting("physics/2d/default_gravity") as float
 
-func _process(delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("MoveLeft", "MoveRight")
-	var viewport_width := get_viewport_rect().size.x
-	var next_x := position.x + direction * move_speed * delta
+	velocity.x = direction * move_speed
 
-	position.x = clampf(next_x, screen_margin, viewport_width - screen_margin)
+	if is_on_floor():
+		if Input.is_action_just_pressed("Jump"):
+			velocity.y = -jump_power
+		else:
+			velocity.y = 0.0
+	else:
+		velocity.y += gravity * delta
+
+	move_and_slide()
+
+	var viewport_width := get_viewport_rect().size.x
+	position.x = clampf(position.x, screen_margin, viewport_width - screen_margin)
